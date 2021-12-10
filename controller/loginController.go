7a7c2414d297e482baa3389169fd06fbd2f93c49
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2021-12-10 16:55:41 +0100

    refactor: move code

--- controller/loginController.go
+++ controller/loginController.go
@@ -5,7 +5,13 @@ import (
 	"example/login/service"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
 
 func LoginUser(c *gin.Context) {
 	loginDto := dto.LoginDto{}
--- controller/userController.go
+++ controller/userController.go
@@ -16,11 +16,6 @@ func HashPassword(password string) (string, error) {
 	return string(bytes), err
 }
 
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
-}
-
 func AddUser(c *gin.Context) {
 	user := model.User{}
 	err := c.BindJSON(&user)
